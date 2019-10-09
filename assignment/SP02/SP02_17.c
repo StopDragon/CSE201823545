be2494bfc4cf1a7f6ee45f685edc4585806fb3dc
StopDragon <jiyong210@gmail.com>
2019-10-09 16:40:48 +0900

    sync

--- assignment/SP02/SP02_17.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    int a = 1, b = 2, c = 3;
-    (a += (b += (c += 7))); /*a += b += c += 7;*/
-    printf("a = %d, b = %d, c = %d\n", a, b, c);
-}
-/* a += b는 a = a + b이므로 a = 13, b = 12, c = 10이라는 값을 출력한다 */
